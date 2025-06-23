43d0cfad5a723a5aa08494b6ebddf64e44a6fd5b
Ramon Work <ramoncostapatel@gmail.com>
2025-06-23 11:58:02 -0700

    Main page template, plus beginning of coursework

---
+++ Portfolio.tsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import "./Portfolio.css"
+
+export class Portfolio extends Component<{}, {}> {
+    constructor(props: {}) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="left">Left</div>
+                <div className="middle">Middle</div>
+                <div className="right">Right</div>
+            </div>
+        );
+    }
+
+    renderCoursework = (): React.ReactElement => {
+        return <div>
+            <h3>Coursework at UW</h3>
+            <ul>
+                <li><a href="http://courses.cs.washington.edu/courses/cse421/">CSE 421: Introduction to Algorithms</a></li>
+            </ul>
+        </div>
+    }
+}
