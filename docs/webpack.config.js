1bcb6e7ae0042ff9c9091c177c62bc48a096a6d8
Ramon Work <ramoncostapatel@gmail.com>
2025-06-23 15:41:09 -0700

    new deployment

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -6,10 +6,6 @@ const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const info = {
-  TITLE: 'Wedding'
-}
-
 const config = {
   mode: 'development',
   devtool: 'source-map',
