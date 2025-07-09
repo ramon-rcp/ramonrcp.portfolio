e018c3b074b47b201603da138cebd74026422a34
Ramon Work <ramoncostapatel@gmail.com>
2025-07-09 12:59:19 -0700

    update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot, Root } from 'react-dom/client';
-import { Portfolio } from './Portfolio';
+// import { Portfolio } from './Portfolio';
+import { App } from './App';
 
 
 const main: HTMLElement|null = document.getElementById('main');
@@ -8,4 +9,4 @@ if (main === null)
   throw new Error("Uh oh! HTML is missing 'main' element");
 
 const root: Root = createRoot(main);
-root.render(<Portfolio/>);
+root.render(<App/>);
