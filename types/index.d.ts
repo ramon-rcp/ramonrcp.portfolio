05c2a5a4f9c1922c44fd124dbcf3791807086f80
ramon-rcp <ramoncostapatel@gmail.com>
2026-07-08 11:41:42 -0700

    add icon element

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,3 +2,13 @@ declare module "*.jpg";
 declare module "*.png";
 declare module "*.pdf";
 declare module "*.css";
+
+declare namespace JSX {
+    interface IntrinsicElements {
+        "iconify-icon": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+            icon?: string;
+            width?: string | number;
+            height?: string | number;
+        };
+    }
+}
